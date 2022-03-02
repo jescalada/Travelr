18eade0fd0c0369777171e917d5c35180c15982d
Juan Escalada <juanescalada175@gmail.com>
2022-03-02 14:01:26 -0800

    Update authentication.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -19,7 +19,12 @@ var uiConfig = {
         if (authResult.additionalUserInfo.isNewUser) {         //if new user
             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                     name: user.displayName,                    //"users" collection
-                    email: user.email                          //with authenticated user's ID (user.uid)
+                    email: user.email,                         //with authenticated user's ID (user.uid)
+                    username: "placeholder",
+                    age: "0",
+                    photo: "",
+                    description: "",
+                    groups: { }
                 }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("./main.html");       //re-direct to main.html after signup
