8cb90c6deb3758b8c776b61957d14a54cacf2a39
Juan Escalada <juanescalada175@gmail.com>
2022-03-02 13:17:32 -0800

    Add main.js for inserting user Name in main page

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {                                                                 
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                  .then(userDoc => {
+               var userName = userDoc.data().name;
+               console.log(userName);
+               //method #1:  insert with html only
+               //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+               //method #2:  insert using jquery
+               $("#navbarDropdownMenuLink").text(userName);                         //using jquery
+            })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertName();
