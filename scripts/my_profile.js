a99803a47c907a36371093ba4781af152ba09bd4
Juan Escalada <juanescalada175@gmail.com>
2022-03-07 16:47:00 -0800

    Add frontend integration for own profile

--- scripts/my_profile.js
+++ scripts/my_profile.js
@@ -1,26 +1,26 @@
 function insertUserData() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
-        if (user) {                                                                 
+        if (user) {
             // Do something for the current logged-in user here: 
             //go to the correct user document by referencing to the user uid
             currentUser = db.collection("users").doc(user.uid);
             //get the document for current user.
             currentUser.get()
-                  .then(userDoc => {
-                let userData = userDoc.data();
-               console.log(`User data: ${JSON.stringify(userDoc.data())}`);
+                .then(userDoc => {
+                    let userData = userDoc.data();
+                    console.log(`User data: ${JSON.stringify(userDoc.data())}`);
 
-               //method #2:  insert using jquery
-               $("#navbarDropdownMenuLink").text(userData.name);
-               $("#description").text(userData.description);
-               $("#email").text(userData.email);
-               $("#location").text(userData.location);
-               $("#status").text(userData.status);
+                    //method #2:  insert using jquery
+                    $("#navbarDropdownMenuLink").text(userData.name);
+                    $("#description").text(userData.description);
+                    $("#email").text(userData.email);
+                    $("#location").text(userData.location);
+                    $("#status").text(userData.status);
+                    $("#username").text("@" + userData.username);
 
-               $("#profile-image").attr("src", userData.photo);
-
-            })
+                    $("#profile-pic").attr("src", userData.photo);
+                })
         } else {
             // No user is signed in.
             // todo Do Something if no user logged in (redirect to login page?)
