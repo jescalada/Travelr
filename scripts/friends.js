fce13f16f369e879a0b8a2494b892260f67ffad1
Stefan Chen <zchen158@my.bcit.ca>
2022-03-23 15:21:13 -0700

    Work with Atsuki on implementing chat function, create friends.html and friends.js
    The chat functionality is in progress. Managed to write the message to the database as a firestore collection. The  chat message was not displayed properly yet.
    
    Co-Authored-By: Atsuki  <87591566+tIcers@users.noreply.github.com>

---
+++ scripts/friends.js
@@ -0,0 +1,28 @@
+//----------------------------------------------
+// Show is list of all users EXCEPT me (who is logged in)
+//----------------------------------------------
+function showAllUsers() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            myid = user.uid;
+            db.collection("users").get()
+                .then(snap => {
+                    snap.forEach(doc => {
+                        if (doc.id != myid) {
+                            let newCard = document.getElementById("card-template")
+                                .content.cloneNode(true);
+                            newCard.querySelector('.card-name').innerHTML = doc.data().name;
+                            newCard.querySelector('.card-href').href = "chat.html?uid=" +
+                                doc.id + "&name=" + doc.data().name;  //pass name and id
+                            document.getElementById("friends-go-here").appendChild(newCard);
+                        }
+                    })
+                })
+
+        } else {
+            // No user is signed in.
+            console.log("no user signed in");
+        }
+    })
+}
+showAllUsers();
