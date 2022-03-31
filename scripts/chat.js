b30e546fd771402f2d85832d2d248b21805616cb
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-03-30 18:07:03 -0700

    Add realtime database chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,25 +1,40 @@
-document.getElementById("message-form").addEventListener("submit", sendMessage);
-
 function sendMessage(e) {
     e.preventDefault();
-  
-    // get values to be submitted
-    const timestamp = Date.now();
-    const messageInput = document.getElementById("message-input");
-    const message = messageInput.value;
-  
-    // clear the input box
-    messageInput.value = "";
-  
-    //auto scroll to bottom
-    document
-      .getElementById("messages")
-      .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-  
-    // create db collection and send in the data
-    db.ref("messages/" + timestamp).set({
-      username,
-      message,
+      
+        // get values to be submitted
+        const timestamp = Date.now();
+        const messageInput = document.getElementById("message-input");
+        const message = messageInput.value;
+      
+        // clear the input box
+        messageInput.value = "";
+      
+        //auto scroll to bottom
+        document
+          .getElementById("messages")
+          .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+      
+        // create db collection and send in the data
+        db.ref("messages/" + timestamp).set({
+          message,
+        });
+      }
+      
+    // add function call to the send button
+    document.getElementById("message-form").addEventListener("submit", sendMessage);
+    
+    // receive message
+    const fetchChat = db.ref("messages/");
+    
+    fetchChat.on("child_added", function (snapshot) {
+    
+    
+      const messages = snapshot.val();
+      const message = `<li><span>${messages.message}</li>`;
+      // class=${userName === messages.userName ? "sent" : "receive"}
+      
+      // append the message on the page
+      document.getElementById("messages").innerHTML += message;
     });
   }
 
