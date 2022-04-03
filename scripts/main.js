b67f0c24412887b9fdd348621966df69144b5483
Stefan Chen <zchen158@my.bcit.ca>
2022-04-02 17:19:30 -0700

    Add keypress events on main.js and search.js to enable search by pressing Enter key

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,9 @@ function search() {
     let locationQuery = $("#location-box").val();
     location.href = "../search.html?location=" + locationQuery;
 }
+
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        search();
+    }
+})
