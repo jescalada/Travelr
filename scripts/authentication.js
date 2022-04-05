512a028025774d71a372fc56678ab567740a5a14
Juan Escalada <juanescalada175@gmail.com>
2022-04-05 16:19:36 -0700

    Refactor authentication.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,73 +1,71 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+// This variable is a default variable defined by Firebase
 var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        //------------------------------------------------------------------------------------------
-        // The code below is modified from default snippet provided by the FB documentation.
-        //
-        // If the user is a "brand new" user, then create a new "user" in your own database.
-        // Assign this user with the name and email provided.
-        // Before this works, you must enable "Firestore" from the firebase console.
-        // The Firestore rules must allow the user to write. 
-        //------------------------------------------------------------------------------------------
-        var user = authResult.user;                            // get the user object from the Firebase authentication database
-        if (authResult.additionalUserInfo.isNewUser) {         //if new user
-          let userName = generateUsername(user.displayName);
-            db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-                    name: user.displayName,                    //"users" collection
-                    email: user.email,                         //with authenticated user's ID (user.uid)
-                    username: userName,
-                    age: "0",
-                    photo: "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg",
-                    status: "I'm ready to Travel!",
-                    description: `Hi, I'm ${user.displayName}. Welcome to my profile!`,
-                    groups: [],
-                    location: "Nowhere"
-                }).then(function () {
-                    console.log("New user added to firestore");
-                    window.location.assign("./main.html");       //re-direct to main.html after signup
-                })
-                .catch(function (error) {
-                    console.log("Error adding new user: " + error);
-                });
-        } else {
-            return true;
-        }
-        return false;
-    },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      //------------------------------------------------------------------------------------------
+      // The code below is modified from default snippet provided by the FB documentation.
+      //
+      // If the user is a "brand new" user, then create a new "user" in your own database.
+      // Assign this user with the name and email provided.
+      // Before this works, you must enable "Firestore" from the firebase console.
+      // The Firestore rules must allow the user to write. 
+      //------------------------------------------------------------------------------------------
+      var user = authResult.user; // Get the user object from the Firebase authentication database
+      if (authResult.additionalUserInfo.isNewUser) { // If new user
+        let userName = generateUsername(user.displayName);  // The username is randomly generated (so it's unique)
+        db.collection("users").doc(user.uid).set({ // Write a new user to database using default values which can then be cahnged
+            name: user.displayName,
+            email: user.email,
+            username: userName,
+            photo: "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg", // Default picture is selected
+            status: "I'm ready to Travel!",
+            description: `Hi, I'm ${user.displayName}. Welcome to my profile!`,
+            groups: [],
+            location: "Nowhere"
+          }).then(function () {
+            console.log("New user added to firestore");
+            window.location.assign("./main.html"); // Redirects to main.html after signup
+          })
+          .catch(function (error) {
+            console.log("Error adding new user: " + error);
+          });
+      } else {
+        return true;
       }
+      return false;
     },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: './main.html',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
+    uiShown: function () {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: './main.html',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
 
-  function generateUsername(displayName) {
-    let trailingNumber = Math.floor(Math.random() * 10000); 
-    displayName = displayName.replace(/\s/g, '').toLowerCase();
-    return displayName + trailingNumber;
-  }
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+// Generates a username for the current user being created. Takes their full name and returns a string with their generated username.
+function generateUsername(displayName) {
+  let trailingNumber = Math.floor(Math.random() * 10000);
+  displayName = displayName.replace(/\s/g, '').toLowerCase();
+  return displayName + trailingNumber;
+}
+
+// Start the firebase UI container
+ui.start('#firebaseui-auth-container', uiConfig);
