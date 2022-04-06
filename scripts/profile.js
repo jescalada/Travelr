0939024a09f27b3216ca37bad9999a9ee9973391
Juan Escalada <juanescalada175@gmail.com>
2022-04-05 19:43:35 -0700

    Refactor profile page, remove edit_profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,50 +1,24 @@
 var currentUser;
 
-// TWO VALID CASES:
-// LOGGED IN USER ACCESSING OWN PROFILE
-// LOGGED IN USER ACCESSING OTHER PROFILE
-// ELSE: RETURN TO LOGIN PAGE
-
-function insertUserData() {
-
+// Loads a user's data into the DOM
+// Two possible cases:
+// Logged in user, accesses own profile
+// Logged in user, accesses someone else's profile
+// ELSE: Redirects to login (handled in load_user page)
+function loadUserData() {
     firebase.auth().onAuthStateChanged(user => {
         let userId = getUserIdFromURL();
-        console.log(userId);
-        console.log(user.uid);
-
         // Check if user is signed in:
         if (user) {
-
-            if (user.uid == userId) {
-                // User is viewing own profile
-
-                currentUser = db.collection("users").doc(user.uid);
-                //get the document for current user.
+            if (user.uid == userId) { // User is viewing own profile
+                currentUser = db.collection("users").doc(user.uid); // Get the document for current user.
                 currentUser.get()
                     .then(userDoc => {
                         let userData = userDoc.data();
                         console.log(`User data: ${JSON.stringify(userData)}`);
-
-                        // We insert the data into the front end using jQuery
-                        $("#profile-full-name").text(userData.name);
-
-                        $("#description").val(userData.description);
-                        $("#email").val(userData.email);
-                        $("#location").val(userData.location);
-
-                        $("#status").text(userData.status);
-
-                        $("#username").text("@" + userData.username);
-
-                        $("#profile-pic").attr("src", userData.photo);
-
-                        $("#edit-profile-name").val(userData.name);
-                        $("#edit-profile-pic").val(userData.photo);
-                        $("#edit-profile-status").val(userData.status);
-            
+                        insertDataIntoDOM(userData);
                         // Append Edit functionality only if current user viewing own profile
                         $("#change-info-buttons").prop('hidden', false);
-
                     })
             } else {
                 // User is logged in, but viewing someone else's profile
@@ -53,37 +27,32 @@ function insertUserData() {
                 currentUser.get()
                     .then(userDoc => {
                         let userData = userDoc.data();
-                        console.log(`User data: ${JSON.stringify(userData)}`);
-
                         // We insert the data into the front end using jQuery
-                        $("#profile-full-name").text(userData.name);
-
-                        $("#description").val(userData.description);
-                        $("#email").val(userData.email);
-                        $("#location").val(userData.location);
-
-                        $("#status").text(userData.status);
-                        $("#username").text("@" + userData.username);
-
-                        $("#profile-pic").attr("src", userData.photo);
-
-                        $("#edit-profile-name").val(userData.name);
-                        $("#edit-profile-pic").val(userData.photo);
-                        $("#edit-profile-status").val(userData.status);
-
+                        insertDataIntoDOM(userData);
                         // Append Edit functionality only if current user viewing own profile
                         $("#change-info-buttons").prop('hidden', true);
-
                     })
             }
-
-        } else {
-            // No user is signed in.
-            // todo Do Something if no user logged in (redirect to login page?)
         }
     });
 }
 
+// Takes user data from JSON and appends it into the DOM
+function insertDataIntoDOM(userData) {
+    $("#profile-full-name").text(userData.name);
+    $("#description").val(userData.description);
+    $("#email").val(userData.email);
+    $("#location").val(userData.location);
+    $("#status").text(userData.status);
+    $("#username").text("@" + userData.username);
+    $("#profile-pic").attr("src", userData.photo);
+    $("#edit-profile-name").val(userData.name);
+    $("#edit-profile-pic").val(userData.photo);
+    $("#edit-profile-status").val(userData.status);
+
+}
+
+// Enables form fields for editing profile
 function editUserInfo() {
     //Enable the form fields
     $("input").prop('disabled', false);
@@ -94,19 +63,25 @@ function editUserInfo() {
 }
 
 function saveUserInfo() {
-    let aboutMe = $("#description").val(); //get the value of the field with id="description"
-    let email = $("#email").val(); //get the value of the field with id="emial"
-    let location = $("#location").val(); //get the value of the field with id="location"
-    
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            updateDatabase(user); // Updates the database only if there's a logged in user (prevents bugs)
+        }
+    });
+}
+
+// Updates the database given a user (from the auth, which has a uid)
+function updateDatabase(user) {
+    // Gets the values of form fields
+    let aboutMe = $("#description").val();
+    let email = $("#email").val();
+    let location = $("#location").val();
     let name = $("#edit-profile-name").val();
     let photo = $("#edit-profile-pic").val();
     let status = $("#edit-profile-status").val();
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            currentUser = db.collection("users").doc(user.uid);
-
-            currentUser.update({
+    currentUser = db.collection("users").doc(user.uid); // Get the doc for the current user
+            currentUser.update({ // Updates the info of the current user using the doc
                     description: aboutMe,
                     email: email,
                     location: location,
@@ -115,30 +90,16 @@ function saveUserInfo() {
                     status: status
                 })
                 .then(() => {
-                    console.log("Document successfully updated!");
-                    $("input").prop('disabled', true);
+                    window.location.href = `profile.html?id=${user.uid}` // Refreshes the page if successfully edited profile
                 })
-        }
-
-    });
-    
-    $("#edit-name-row").prop("hidden", true);
-    $("#edit-profile-pic-row").prop("hidden", true);
-    $("#edit-status-row").prop("hidden", true);
-
-    $("#profile-pic").attr("src", photo);
-    $("#profile-full-name").text(name);
-    $("#status").text(status);
-
-
 }
 
+// Gets the user's ID from the URL
 function getUserIdFromURL() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     return urlParams.get('id');
 }
 
-insertUserData();
-
-
+// Automatically loads the user data
+loadUserData();
